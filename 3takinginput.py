1f158ebd75fe4a26ee3ff375435a17b631a6823a
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-02 11:52:57 +0545

    Updated favourite color

--- 3takinginput.py
+++ 3takinginput.py
@@ -1,4 +1,5 @@
 name = input("What is your name?")
 age = input("What is your age?")
+fav_color = input("What is your favourite color?")
 
-print("You are "+ name +" and you are "+str(age) +" years old")
+print("You are "+ name +" and you are "+str(age) +" years old and you like "+fav_color+" color")
