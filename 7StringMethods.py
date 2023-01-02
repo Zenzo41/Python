bcc1dfab4c31cf2d58ab939484b10ec00f0d192d
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-02 21:18:06 +0545

    Initial Commit

---
+++ 7StringMethods.py
@@ -0,0 +1,14 @@
+text = 'Python'
+print(len(text))
+
+print('In Uppercase: '+text.upper())
+print('In Lowercase: '+text.upper())
+
+print('The index h is in the text: '+str(text.find('h')))
+
+print('Replacing Python with Java:  '+text.replace('Python','Java'))
+
+print('h' in text)
+
+#learnt these methods : len() , text.upper() , text.upper()
+#text.find('h') , text.replace('Python','Java') , ('h' in text)
