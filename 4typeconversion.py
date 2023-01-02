97c12d0c53a540fe3f980dbebd1f1e3c5556c4fc
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-02 12:01:44 +0545

    Added Simulation One and Two

--- 4typeconversion.py
+++ 4typeconversion.py
@@ -1,3 +1,11 @@
+#Simulation One:
 birth_year = input("Birth Year")
 age = 2023 - int(birth_year)
 print("You are "+str(age)+" years old")
+
+print(type(birth_year))
+
+#Simulation Two:
+weight_pound = input("Enter your weight in pound: ")
+weight_kg = int(weight_pound) * 0.45
+print("Your weight is "+str(weight_kg)+" kg")
