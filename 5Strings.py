9137101071c77241c9da23eade1ca6ed1d821a14
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-02 21:00:45 +0545

    Used Index to print

---
+++ 5Strings.py
@@ -0,0 +1,17 @@
+course ='Python for "Beginners"'
+print(course)
+print(course[0:5])
+
+course1 ='Python for Beginners'
+print(course1)
+print(course1[:6])
+
+DemoLetter = '''
+Hi John,
+Here is our first mail to you.
+
+Thank you,
+The Support Team
+'''
+print(DemoLetter)
+print(DemoLetter[5:-15])
