f6acdddfb23b0d8c8597ce517db8958eef73486b
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-03 08:46:45 +0545

    Intial Commit

---
+++ 8ArithmeticOperators.py
@@ -0,0 +1,9 @@
+x=10
+x += 3
+print(x)
+
+print(10/3)  #Divides and returns the decimal too
+print(10//3) #Divides and returns the integer only
+print(10%3)  #Returns the remainder
+print(10**3) #Returns the power of 3
+
