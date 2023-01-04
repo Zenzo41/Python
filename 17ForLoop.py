b4ba3d3f1732c458b5bd21c0312551189910b3b9
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-04 09:06:37 +0545

    Initial Commit

---
+++ 17ForLoop.py
@@ -0,0 +1,13 @@
+for item in 'Python':
+    print(item)
+    
+for item in ['Zen', 'Zenzo' ,'Zamazenta']:
+    print(item)
+ 
+    
+prices = [10,20,30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total : {total}")
+    
