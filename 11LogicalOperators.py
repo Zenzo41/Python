4b46e712ee2bfb8f121641264c767fc4d5603c47
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-04 06:31:58 +0545

    Added another condition

--- 11LogicalOperators.py
+++ 11LogicalOperators.py
@@ -1,7 +1,14 @@
 high_income = True
 good_credit = True
+has_criminalRecord =False
 
 if high_income and good_credit:
+    print("You are eligible for Loan")
+else:
+    print("You are not eligible for Loan")
+
+
+if good_credit and not has_criminalRecord:
     print("You are eligible for Loan")
 else:
     print("You are not eligible for Loan")
