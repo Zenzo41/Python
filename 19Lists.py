78dfac1b343bf35d16793230972d10ccac325454
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-21 17:49:35 +0545

    Initial Commit

---
+++ 19Lists.py
@@ -0,0 +1,12 @@
+names = ['John','Bob','Sarah','Mary','Mosh']
+print(names[2]) #INDIVIDUAL VALUE ACCESSED
+
+print(names[2:]) #from 2nd index to the last
+
+print(names[1:3])# from 1 to 3rd index
+
+print(names)
+
+names[0] = 'Jon'
+
+print(names)
