b980218f6c49a73d3e4a5dd6da62fc6530304418
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-21 18:40:27 +0545

    Methods in List

---
+++ 21ListMethods.py
@@ -0,0 +1,35 @@
+numbers = [1,2,3,4,5]
+print(numbers)
+
+numbers.append(7)
+print(numbers)
+
+numbers.insert(0,-1)
+print(numbers)
+
+numbers.remove(5)
+print(numbers)
+
+numbers.pop()
+print(numbers)
+
+numbers.insert(5,5)
+print(numbers)
+
+numbers.clear()
+print(numbers)
+
+values = [5,2,1,7,4]
+print(5 in values)
+print(values.index(2))
+
+values.sort()
+print(values)
+
+values.reverse()
+print(values)
+
+value1 = values.copy()
+value1.append(22)
+print(value1)
+
