ebd2ea38334b246fd47a04718d686f4fc42b47a9
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-21 18:43:57 +0545

    remove duplicates

--- 21ListMethods.py
+++ 21ListMethods.py
@@ -1,35 +1,43 @@
-numbers = [1,2,3,4,5]
-print(numbers)
+# numbers = [1,2,3,4,5]
+# print(numbers)
 
-numbers.append(7)
-print(numbers)
+# numbers.append(7)
+# print(numbers)
 
-numbers.insert(0,-1)
-print(numbers)
+# numbers.insert(0,-1)
+# print(numbers)
 
-numbers.remove(5)
-print(numbers)
+# numbers.remove(5)
+# print(numbers)
 
-numbers.pop()
-print(numbers)
+# numbers.pop()
+# print(numbers)
 
-numbers.insert(5,5)
-print(numbers)
+# numbers.insert(5,5)
+# print(numbers)
 
-numbers.clear()
-print(numbers)
+# numbers.clear()
+# print(numbers)
 
-values = [5,2,1,7,4]
-print(5 in values)
-print(values.index(2))
+# values = [5,2,1,7,4]
+# print(5 in values)
+# print(values.index(2))
 
-values.sort()
-print(values)
+# values.sort()
+# print(values)
 
-values.reverse()
-print(values)
+# values.reverse()
+# print(values)
 
-value1 = values.copy()
-value1.append(22)
-print(value1)
+# value1 = values.copy()
+# value1.append(22)
+# print(value1)
 
+# Program to remove duplicates in a list
+numbers = [2,2,4,6,3,4,6,1]
+uniques=[]
+
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)
