7278748e8c1f3b2c289041add3e37ba343d50bec
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-21 17:52:45 +0545

    Largest Number in a List

--- 19Lists.py
+++ 19Lists.py
@@ -1,12 +1,22 @@
-names = ['John','Bob','Sarah','Mary','Mosh']
-print(names[2]) #INDIVIDUAL VALUE ACCESSED
+# names = ['John','Bob','Sarah','Mary','Mosh']
+# print(names[2]) #INDIVIDUAL VALUE ACCESSED
 
-print(names[2:]) #from 2nd index to the last
+# print(names[2:]) #from 2nd index to the last
 
-print(names[1:3])# from 1 to 3rd index
+# print(names[1:3])# from 1 to 3rd index
 
-print(names)
+# print(names)
 
-names[0] = 'Jon'
+# names[0] = 'Jon'
 
-print(names)
+# print(names)
+
+
+#Largest Number in a List
+numbers = [1,2,3,4,5,6]
+max = numbers[0]
+
+for i in numbers:
+    if i > max:
+        max = i
+print(max)
