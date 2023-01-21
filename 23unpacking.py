dfbee382dee21354ce8b3db265a4da34e0336967
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-21 20:34:02 +0545

    Unpacking in Python

---
+++ 23unpacking.py
@@ -0,0 +1,15 @@
+coordinates = (1,2,3)
+
+x,y,z = coordinates
+
+print(y)
+
+# Tuple works by assigning first item to x , assigning second item to y,
+#assigning third item to z
+
+
+# OR
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
