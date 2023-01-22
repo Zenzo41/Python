dc5b5bc5dd090a289559ce75e95e9c5164def1e7
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-22 13:48:40 +0545

    Initial Commit

---
+++ 26Functions.py
@@ -0,0 +1,16 @@
+def greet_user():
+    print("Hi there")
+    print("Welcome aboard")
+    
+
+print("Start")
+greet_user()
+print("Finish")
+def kick_user():
+    print("You have to get outta here")
+    print("You are not allowed here")
+    
+    
+kick_user()
+
+#define function first and then only use them 
