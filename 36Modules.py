bfce9a6a5f698ce01fe52db67e07ebb18153f772
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-24 20:29:25 +0545

    Addedthewayto import specific function from module

--- 36Modules.py
+++ 36Modules.py
@@ -1,3 +1,10 @@
+#import all the function from the module
 import converter_module
 
 print(converter_module.kg_to_lbs(70))
+
+#import specific function from the module
+import converter_module
+from converter_module import kg_to_lbs
+
+print(kg_to_lbs(100))
