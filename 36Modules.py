fe49fed0d748f8f6af0c6af1a7e5c8589e2ccfb8
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-24 20:40:37 +0545

    UpdatedTheCodeAfterAddingThe converter in utils.py

--- 36Modules.py
+++ 36Modules.py
@@ -1,10 +1,10 @@
 #import all the function from the module
-import converter_module
+import utils
 
-print(converter_module.kg_to_lbs(70))
+print(utils.kg_to_lbs(70))
 
 #import specific function from the module
-import converter_module
-from converter_module import kg_to_lbs
+import utils
+from utils import kg_to_lbs
 
 print(kg_to_lbs(100))
