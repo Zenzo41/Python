36f0eefb01c519d17bfa345f36fe46bb57c82e32
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-29 20:25:44 +0545

    updated the transactions.xlsx by removing $

--- Project01_ProcessingExcelSpreadsheet/app.py
+++ Project01_ProcessingExcelSpreadsheet/app.py
@@ -6,12 +6,11 @@ sheet = wb['Sheet1']
 
 cell = sheet['a1'] # or cell = sheet.cell(1,1)
 
-for row in range(2,sheet.max_row+1):
-   cell = sheet.cell(row,3)
+for row in range(2, sheet.max_row + 1):
+   cell = sheet.cell(row, 3)
    corrected_price = cell.value * 0.9
-   corrected_price_cell = sheet.cell(row,4)
+   corrected_price_cell = sheet.cell(row, 4)
    corrected_price_cell.value = corrected_price
-   
    
 wb.save('transactions2.xlsx')
 
